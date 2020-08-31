b7317a303547a33d55e5b2ce7e0e1738cb2ad22b
AlexeyAnufriev <contact@alexey-anufriev.com>
2020-08-31 21:19:33 +0200

    chore: define actions group base

---
+++ src/main/kotlin/com/alexey_anufriev/scopes_manager/actions/ScopeGroupActionBase.kt
@@ -0,0 +1,62 @@
+package com.alexey_anufriev.scopes_manager.actions
+
+import com.alexey_anufriev.scopes_manager.utils.ActionUtils.hideAction
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.packageDependencies.DependencyValidationManager
+import com.intellij.psi.search.scope.packageSet.NamedScopeManager
+import com.intellij.psi.search.scope.packageSet.NamedScopesHolder
+import java.util.Arrays
+import java.util.Objects
+
+abstract class ScopeGroupActionBase : DefaultActionGroup() {
+
+    /**
+     * Checks if the action group is eligible to be displayed
+     */
+    override fun update(event: AnActionEvent) {
+        val project = event.project
+
+        if (project == null) {
+            hideAction(event)
+            return
+        }
+
+        val fileIndex = ProjectRootManager.getInstance(project).fileIndex
+
+        val selectionContainsNoProjectFiles = Arrays.stream(event.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY))
+            .map(fileIndex::getContentRootForFile)
+            .allMatch(Objects::isNull)
+
+        if (selectionContainsNoProjectFiles) {
+            hideAction(event)
+        }
+    }
+
+    /**
+     * Defines nested items of the action group
+     */
+    override fun getChildren(event: AnActionEvent?): Array<AnAction> {
+        if (event == null) {
+            return emptyArray()
+        }
+
+        val project = event.project ?: return emptyArray()
+        val localScopesManager = NamedScopeManager.getInstance(project)
+        val sharedScopesManager = DependencyValidationManager.getInstance(project)
+
+        return defineChildItems(project, event, localScopesManager, sharedScopesManager)
+    }
+
+    protected abstract fun defineChildItems(
+        project: Project,
+        event: AnActionEvent,
+        localScopesManager: NamedScopesHolder,
+        sharedScopesManager: NamedScopesHolder
+    ): Array<AnAction>
+
+}
