79cedd7476aa9d48bc2c30f22af3b4abb5481a16
AlexeyAnufriev <contact@alexey-anufriev.com>
2020-08-31 23:59:09 +0200

    feat: assign shortcuts to add/remove actions

---
+++ src/main/kotlin/com/alexey_anufriev/scopes_manager/ScopesManagerInitializer.kt
@@ -0,0 +1,27 @@
+package com.alexey_anufriev.scopes_manager
+
+import com.intellij.openapi.actionSystem.KeyboardShortcut
+import com.intellij.openapi.keymap.ex.KeymapManagerEx
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import java.awt.event.InputEvent
+import java.awt.event.KeyEvent
+import javax.swing.KeyStroke
+
+class ScopesManagerInitializer : StartupActivity {
+
+    override fun runActivity(project: Project) {
+        val activeKeymap = KeymapManagerEx.getInstanceEx().activeKeymap
+
+        activeKeymap.addShortcut(
+            "com.alexey-anufriev.scopes-manager.AddToScopeActionGroup",
+            KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_ADD, InputEvent.ALT_DOWN_MASK), null)
+        )
+
+        activeKeymap.addShortcut(
+            "com.alexey-anufriev.scopes-manager.RemoveFromScopeActionGroup",
+            KeyboardShortcut(KeyStroke.getKeyStroke(KeyEvent.VK_SUBTRACT, InputEvent.ALT_DOWN_MASK), null)
+        )
+    }
+
+}
