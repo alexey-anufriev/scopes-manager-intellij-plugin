17881079c22a49c7776af840114bf765cedbb732
alexey-anufriev <ice.sumy@gmail.com>
2024-04-03 02:21:02 +0200

    feat: support save/restore of project view

--- src/main/kotlin/com/alexey_anufriev/scopes_manager/actions/ScopeActionBase.kt
+++ src/main/kotlin/com/alexey_anufriev/scopes_manager/actions/ScopeActionBase.kt
@@ -1,6 +1,8 @@
 package com.alexey_anufriev.scopes_manager.actions
 
 import com.intellij.ide.projectView.ProjectView
+import com.intellij.ide.projectView.impl.ProjectViewPane
+import com.intellij.ide.scopeView.ScopeViewPane
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -9,6 +11,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.wm.ToolWindowId.PROJECT_VIEW
+import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.packageDependencies.ui.DirectoryNode
 import com.intellij.packageDependencies.ui.FileNode
 import com.intellij.packageDependencies.ui.ModuleNode
@@ -19,7 +23,9 @@ import com.intellij.psi.search.scope.packageSet.NamedScope
 import com.intellij.psi.search.scope.packageSet.NamedScopesHolder
 import com.intellij.psi.search.scope.packageSet.PackageSet
 import com.intellij.psi.search.scope.packageSet.PackageSetBase
+import com.intellij.util.concurrency.EdtExecutorService
 import java.util.Arrays
+import java.util.concurrent.TimeUnit
 import java.util.stream.Collectors
 import java.util.stream.Stream
 
@@ -46,7 +52,30 @@ abstract class ScopeActionBase(
             .filter { file -> projectManager.fileIndex.isInContent(file) }
             .forEach { file -> performScopeAction(project, projectManager, patternProvider, unchangedScopes, file) }
 
-        ProjectView.getInstance(project).refresh()
+        // save and restore view (e.g. when a certain scope was selected)
+        val contentManager = ToolWindowManager.getInstance(project).getToolWindow(PROJECT_VIEW)?.contentManager
+        val projectView = ProjectView.getInstance(project)
+        val viewId = projectView.currentProjectViewPane.id
+        var selectScopeName: String? = null
+
+        if (viewId == ScopeViewPane.ID && contentManager != null) {
+            selectScopeName = contentManager.selectedContent?.displayName
+        }
+
+        // switch to main project tree for global refresh first
+        projectView.changeView(ProjectViewPane.ID)
+        projectView.refresh()
+
+        // then switch back to scope that was selected
+        if (selectScopeName != null && contentManager != null) {
+            val restoreScopeAction: () -> Unit = {
+                val selectedScope = contentManager.contents.first { content -> content.displayName == selectScopeName }
+                contentManager.setSelectedContent(selectedScope)
+            }
+
+            // wait for a delay to let project view perform a refresh after scope content change
+            EdtExecutorService.getScheduledExecutorInstance().schedule(restoreScopeAction, 25, TimeUnit.MILLISECONDS);
+        }
     }
 
     private fun performScopeAction(
